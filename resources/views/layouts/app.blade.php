0b431ff718f42af86966ce1e1343ffa92ad6a428
Putra <risangputra144@gmail.com>
2026-01-23 09:39:00 +0700

    analis google

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,14 +1,20 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
-        <!-- Google Tag Manager -->
-        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-TRKQN4BR');</script>
-        <!-- End Google Tag Manager -->
-        
+     @if(app()->environment('production'))
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-TRKQN4BR');</script>
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-RCMW2MH8LW"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'G-RCMW2MH8LW');
+    </script>
+      @endif
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
@@ -434,10 +440,10 @@ body {
     </head>
     
     <body class="font-sans antialiased">
-      <!-- Google Tag Manager (noscript) -->
-      <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TRKQN4BR"
-      height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-      <!-- End Google Tag Manager (noscript) -->
+      @if(app()->environment('production'))
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TRKQN4BR"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    @endif
 
         {{-- Jika Anda masih menggunakan div#splash dan div#loader di body, 
              pastikan Anda menghapusnya untuk sementara waktu agar konten utama (@yield('content')) terlihat --}}
